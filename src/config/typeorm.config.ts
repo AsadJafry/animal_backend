de6acd8aeb0cf5e1da24ad5f798c29d41a265f54
asadjafry <asadj2000@gmail.com>
2023-09-06 18:26:51 +0500

    changed config for ec2

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,10 +2,13 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig:TypeOrmModuleOptions={
 
     type:'postgres',
+    //host:'localhost',
     host:'localhost',
     port:5432,
-    username:'admin',
-    password:'12345678',
+    //username:'admin',
+    username:'postgres',
+    //password:'12345678',
+    password:'password',
     database:'animalapp',
     entities:[__dirname + '/../**/*.entity{.ts,.js}'], 
     synchronize:true,
